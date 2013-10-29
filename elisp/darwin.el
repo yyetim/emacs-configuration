0775a5af53fe717ed2d02bdcfe6a7a945fa520e2
Yavuz Yetim <yyetim@nat-oitwireless-inside-vapornet100-a-10276.princeton.edu>
2013-10-28 21:35:33 -0400

    changed os x font from 110 to 130

--- elisp/darwin.el
+++ elisp/darwin.el
@@ -3,7 +3,7 @@
   ;; If you edit it by hand, you could mess it up, so be careful.
   ;; Your init file should contain only one such instance.
   ;; If there is more than one, they won't work right.
- '(default ((t (:height 110 :family "Menlo" :foundry "apple")))))
+ '(default ((t (:height 130 :family "Menlo" :foundry "apple")))))
 
 (add-to-list 'exec-path "/usr/local/bin")
 (setq ispell-program-name "aspell"
