0c2889eefcda2134a39efb20419b78b737cc9b9f
Ozlem Bilgir <obilgir@nat-oitwireless-inside-vapornet100-c-6394.princeton.edu>
2014-03-15 21:32:15 -0400

    mac os x font

--- elisp/darwin.el
+++ elisp/darwin.el
@@ -3,7 +3,7 @@
   ;; If you edit it by hand, you could mess it up, so be careful.
   ;; Your init file should contain only one such instance.
   ;; If there is more than one, they won't work right.
- '(default ((t (:height 130 :family "Menlo" :foundry "apple")))))
+ '(default ((t (:height 120 :family "Menlo" :foundry "apple")))))
 (setq ispell-program-name "aspell")
 
 (add-to-list 'exec-path "/usr/local/bin")
