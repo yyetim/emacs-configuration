6788eeba73aba73b915ec57ae2ec61f45961c1d5
yyetim <yyetim@gmail.com>
2014-08-01 10:06:26 -0400

    Update windows-nt.el

--- elisp/windows-nt.el
+++ elisp/windows-nt.el
@@ -3,7 +3,7 @@
 (setenv "PATH" (concat "c:/cygwin/bin;" (getenv "PATH")))
 (setq exec-path (cons "c:/cygwin/bin/" exec-path))
 
-(setq tramp-default-method "plink")
+(setq tramp-default-method "psftp")
 ;; (require 'cygwin-mount)
 ;; (cygwin-mount-activate)
 
