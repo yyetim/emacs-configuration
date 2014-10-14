8120674856096572d3cc969fdc369dc76d18bfa1
Yavuz Yetim <yyetim@google.com>
2014-10-14 09:46:06 -0700

    font size

--- elisp/gnu/linux.el
+++ elisp/gnu/linux.el
@@ -1,5 +1,5 @@
 (custom-set-faces
-'(default ((t (:height 100 :family "Ubuntu Mono")))))
+'(default ((t (:height 110 :family "Ubuntu Mono")))))
 
 (setq x-select-enable-clipboard t)
 (menu-bar-mode -1)
